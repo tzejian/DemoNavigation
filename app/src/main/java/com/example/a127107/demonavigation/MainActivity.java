14718f2ade73966b2c03cb7225814fe273207fda
tzejian <akernus.lr@gmail.com>
2017-08-09 00:25:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a127107/demonavigation/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.a127107.demonavigation;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnNewActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnNewActivity = (Button)
+                this.findViewById(R.id.btnNewActivity);
+
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,
+                        NewActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/a127107/demonavigation/NewActivity.java
@@ -0,0 +1,19 @@
+package com.example.a127107.demonavigation;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewActivity extends AppCompatActivity {
+
+    ActionBar ab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+
+        ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+}
